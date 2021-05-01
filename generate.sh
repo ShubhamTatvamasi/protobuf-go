010a028c61362c5bd5a76702dfdce18f4f48861c
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-05-01 18:36:23 +0530

    modified:   main.go

--- generate.sh
+++ generate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Hello"
+# stop script if there are some errors
+set -e
+
+echo "Generating Proto Buffer code for golang..."
 protoc -I pkg/ --go_out=pkg/ pkg/simple/simple.proto 
 
-
-
+echo "Done"
--- main.go
+++ main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ShubhamTatvamasi/protobuf-go/pkg/simple"
+	"github.com/golang/protobuf/proto"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	sm := doSimple()
+
+	readAndWriteDemo(sm)
+
+}
+
+func readAndWriteDemo(sm proto.Message) {
+
+	writeToFile("simple.bin", sm)
+
+	sm2 := &simple.SimpleMessage{}
+
+	readFromFile("simple.bin", sm2)
+	fmt.Println("Read the content:", sm2)
+}
+
+func writeToFile(fname string, pb proto.Message) error {
+	out, _ := proto.Marshal(pb)
+
+	if err := os.WriteFile(fname, out, 0644); err != nil {
+		log.Fatalln("Can't write to file")
+		return err
+	}
+
+	fmt.Println("Data has been written!")
+	return nil
+}
+
+func readFromFile(fname string, pb proto.Message) error {
+	in, _ := os.ReadFile(fname)
+
+	if err := proto.Unmarshal(in, pb); err != nil {
+		log.Fatalln("Couldn't put the bytes into the protocol struct", err)
+		return err
+	}
+
+	return nil
+}
+
+func doSimple() *simple.SimpleMessage {
+
+	sm := simple.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "My Simple Message",
+		SampleList: []int32{1, 4, 7, 8},
+	}
+
+	fmt.Println(sm)
+
+	sm.Name = "New name"
+
+	fmt.Println("The Name is:", sm.GetName())
+	fmt.Println("The Id is:", sm.GetId())
+
+	return &sm
+
 }
